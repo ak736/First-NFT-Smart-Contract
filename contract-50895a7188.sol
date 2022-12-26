6e2393a1bf0e9a047464fe11b2e48502393ca8f7
Aniket Kumar <as0700@srmist.edu.in>
2022-12-26 21:15:00 +0530

    Update contract-50895a7188.sol

--- contract-50895a7188.sol
+++ contract-50895a7188.sol
@@ -2,34 +2,26 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
 
-contract Jinxxx is ERC721, ERC721Enumerable, ERC721URIStorage{
+contract Jinx is ERC721, ERC721URIStorage{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    uint256 MAX_SUPPLY = 10000;
+    uint256 MAX_SUPPLY = 100000;
 
-    constructor() ERC721("Jinxxx", "JINX") {}
+    constructor() ERC721("Jinx", "JINX") {}
 
-    function safeMint(address to, string memory uri) public{
+    function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
-        require(tokenId  <= MAX_SUPPLY, "I'm Sorry, All NFTs have been minted.");
+        require(_tokenIdCounter.current() <= MAX_SUPPLY, "I'm sorry we reached the cap");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
@@ -43,13 +35,5 @@ contract Jinxxx is ERC721, ERC721Enumerable, ERC721URIStorage{
     {
         return super.tokenURI(tokenId);
     }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
+
